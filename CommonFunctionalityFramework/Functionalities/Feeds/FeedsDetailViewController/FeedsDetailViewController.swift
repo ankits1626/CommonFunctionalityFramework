bd0b2550df617f16faefce61afb99f6721032e77
Rewardz <ankit._seeking_job@yahoo.co.in>
2020-03-06 14:37:26 +0530

    Added comment bar on feed detail

--- CommonFunctionalityFramework/Functionalities/Feeds/FeedsDetailViewController/FeedsDetailViewController.swift
+++ CommonFunctionalityFramework/Functionalities/Feeds/FeedsDetailViewController/FeedsDetailViewController.swift
@@ -9,11 +9,31 @@
 import UIKit
 
 class FeedsDetailViewController: UIViewController {
-
+    @IBOutlet weak var commentBarView : ASChatBarview?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+}
+
+extension FeedsDetailViewController : ASChatBarViewDelegate{
+    func finishedPresentingOverKeyboard() {
+        
+    }
+    
+    func addAttachmentButtonPressed() {
+        
+    }
+    
+    func removeAttachment() {
+        
+    }
+    
+    func rightButtonPressed(_ chatBar: ASChatBarview) {
+        
+    }
+    
+    
 }
