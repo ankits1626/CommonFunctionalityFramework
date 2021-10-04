c343781922154def20376fd559188e7e253b3f67
Suyesh Kandpal <suyesh.kandpal@stu.upes.ac.in>
2021-10-04 10:24:28 +0530

    worker added

---
+++ CommonFunctionalityFramework/Functionalities/Feeds/ReportAbuseConfirmationDrawer/Worker/ReportAbuseWorker.swift
@@ -0,0 +1,68 @@
+//
+//  ReportAbuseWorker.swift
+//  CommonFunctionalityFramework
+//
+//  Created by Ankit Sachan on 11/10/20.
+//  Copyright © 2020 Rewardz. All rights reserved.
+//
+
+import Foundation
+typealias ReportAbuseWorkerResultHandler = (APICallResult<Any?>) -> Void
+
+class ReportAbuseWorker  {
+    typealias ResultType = Any?
+    var commonAPICall : CommonAPICall<ReportAbuseDataParser>?
+    private let networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol
+    init(networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol) {
+        self.networkRequestCoordinator = networkRequestCoordinator
+    }
+    func reportAbusePost(_ feedIdentifier: Int64, notes: String?, completionHandler: @escaping ReportAbuseWorkerResultHandler) {
+        if (commonAPICall == nil){
+            self.commonAPICall = CommonAPICall(
+                apiRequestProvider: ReportAbuseRequestGenerator(feedIdentifier: feedIdentifier, notes: notes, networkRequestCoordinator: networkRequestCoordinator),
+                dataParser: ReportAbuseDataParser(),
+                logouthandler: networkRequestCoordinator.getLogoutHandler()
+            )
+        }
+        commonAPICall?.callAPI(completionHandler: { (result : APICallResult<ResultType>) in
+            completionHandler(result)
+        })
+    }
+}
+
+class ReportAbuseRequestGenerator: APIRequestGeneratorProtocol  {
+    var urlBuilder: ParameterizedURLBuilder
+    var requestBuilder: APIRequestBuilderProtocol
+    private let networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol
+    var feedIdentifier : Int64
+    var notes: String?
+    init( feedIdentifier: Int64, notes: String?, networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol) {
+        self.feedIdentifier = feedIdentifier
+        self.notes = notes
+        self.networkRequestCoordinator = networkRequestCoordinator
+        urlBuilder = ParameterizedURLBuilder(baseURLProvider: networkRequestCoordinator.getBaseUrlProvider())
+        requestBuilder = APIRequestBuilder(tokenProvider: networkRequestCoordinator.getTokenProvider())
+    }
+    var apiRequest: URLRequest?{
+        get{
+            if let baseUrl = networkRequestCoordinator.getBaseUrlProvider().baseURLString(){
+                let req =  self.requestBuilder.apiRequestWithHttpParamsAggregatedHttpParams(
+                    url: URL(string: baseUrl + "feeds/api/posts/\(feedIdentifier)/flag/"),
+                    method: .POST,
+                    httpBodyDict: notes == nil ? nil :  ["notes" : notes!]
+                )
+                return req
+            }
+            return nil
+        }
+    }
+}
+
+class ReportAbuseDataParser: DataParserProtocol {
+    typealias ExpectedRawDataType = Any?
+    typealias ResultType = Any?
+    
+    func parseFetchedData(fetchedData: ExpectedRawDataType) -> APICallResult<ResultType> {
+        return .Success(result: nil)
+    }
+}
