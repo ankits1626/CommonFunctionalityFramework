c67b71e0d4f3a7c85e525d13227103ddf4f7fa7d
suyeshkandpal28 <suyesh.kandpal@stu.upes.ac.in>
2022-07-21 18:45:34 +0530

    nomination changes

--- CommonFunctionalityFramework/Functionalities/CommonFeeeds/Nomination/BOUSNominationViewController.swift
+++ CommonFunctionalityFramework/Functionalities/CommonFeeeds/Nomination/BOUSNominationViewController.swift
@@ -1,40 +1,34 @@
 //
-//  BOUSApprovalsListingViewController.swift
+//  BOUSNominationViewController.swift
 //  CommonFunctionalityFramework
 //
-//  Created by Puneeeth on 13/07/22.
+//  Created by Suyesh Kandpal on 21/07/22.
 //  Copyright © 2022 Rewardz. All rights reserved.
 //
 
 import UIKit
+import RewardzCommonComponents
 
-class BOUSApprovalsListingViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+class BOUSNominationViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
-    @IBOutlet weak var approvalsCountHolder: NSLayoutConstraint!
     @IBOutlet weak var selectAllBtn: UIButton!
-    @IBOutlet weak var approvalsCountLbl: UILabel!
     @IBOutlet weak var tableView: UITableView!
     var requestCoordinator: CFFNetworkRequestCoordinatorProtocol!
     var jsonDataValues = [BOUSApprovalDataResponseValues]()
     var loader = MFLoader()
     var mediaFetcher: CFFMediaCoordinatorProtocol!
+    var statusType : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        approvalsCountLbl.text = ""
         loadApprovalsList()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.tabBarController?.tabBar.isHidden = false
-        NotificationCenter.default.post(name: Notification.Name(rawValue: "showMenuButton"), object: nil)
-    }
-    
+        
     func loadApprovalsList(){
-        loader.showActivityIndicator(view)
-        BOUSGetApprovalsListWorker(networkRequestCoordinator: requestCoordinator).getApprovalsList(searchString: "", nextUrl: "") { (result) in
+        self.view.showBlurLoader()
+        GetBOUSNominationWorker(networkRequestCoordinator: requestCoordinator).getNominationList(statusType: statusType, nextUrl: "")  { (result) in
             DispatchQueue.main.async {
-                self.loader.hideActivityIndicator(self.view)
+                self.view.removeBluerLoader()
                 switch result{
                 case .Success(result: let response):
                     if let dataValue = response["results"] as? NSArray {
@@ -60,9 +54,6 @@ class BOUSApprovalsListingViewController: UIViewController, UITableViewDelegate,
             let decoder = JSONDecoder()
             let jsonData = try decoder.decode(BOUSApprovalData.self, from: data)
              jsonDataValues =  jsonData.results
-//            arrayHolder = []
-//            arrayHolder = jsonData.results
-            approvalsCountLbl.text = "\(jsonDataValues.count) Approvals Pending"
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
@@ -78,27 +69,67 @@ class BOUSApprovalsListingViewController: UIViewController, UITableViewDelegate,
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! BOUSApprovalsListTableViewCell
         let dataValue = jsonDataValues[indexPath.row]
+        if !dataValue.nomination.nominated_team_member.profile_img.isEmpty{
+            mediaFetcher.fetchImageAndLoad(cell.usrImg, imageEndPoint: dataValue.nomination.nominated_team_member.profile_img)
+        }else{
+            cell.usrImg.image = nil
+        }
         
-        mediaFetcher.fetchImageAndLoad(cell.usrImg, imageEndPoint: dataValue.nomination.nominated_team_member.profile_img ?? "")
+        if !dataValue.nomination.badges.icon.isEmpty{
+            mediaFetcher.fetchImageAndLoad(cell.awardThumbnail, imageEndPoint: dataValue.nomination.badges.icon)
+        }else{
+            cell.awardThumbnail.image = nil
+        }
         
-        cell.nominatedUserName.text = "\(dataValue.nomination.nominated_team_member.full_name) nominated"
-        mediaFetcher.fetchImageAndLoad(cell.awardThumbnail, imageEndPoint: dataValue.nomination.badges.icon ?? "")
-        cell.nominatedBy.text = "\(dataValue.nomination.user_strength.name)"
-        cell.awardType.text = "\(dataValue.nomination.badges.name)"
-        cell.nominatedBy.text = "by \(dataValue.nomination.nominator_name)"
-        cell.nominatedDate.text = "\(dataValue.nomination.created)"
-        cell.timeRemaining.text = "\(dataValue.time_left)"
-        cell.awardPoints.text = "\(dataValue.nomination.badges.award_points)"
+        
+        cell.nominatedUserName.attributedText =  getCreatorName(text: "For", userName: "\(dataValue.nomination.nominator_name)")
+        cell.nominatedDate.text = getCreationDate(jsonData: dataValue)
         cell.userStrengthTitle.text = "\(dataValue.nomination.user_strength.name)"
         cell.userStrengthDescription.text = "\(dataValue.nomination.user_strength.message)"
+        cell.awardType.text = "\(dataValue.nomination.badges.name)"
+        cell.awardPoints.text = "\(dataValue.nomination.badges.award_points) Points"
+        cell.timeRemaining.text = "\(dataValue.nomination.nom_status)"
+        cell.timeRemaining.curvedWithoutBorderedControl(borderColor: .clear, borderWidth: 1.0, cornerRadius: 6.0)
+        cell.timeRemaining.backgroundColor = Rgbconverter.HexToColor("#FFA412", alpha: 0.1)
+        cell.timeRemaining.textColor = Rgbconverter.HexToColor("#FFA412", alpha: 1.0)
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 202
+    }
+    func getCreatorName(text : String , userName : String) -> NSAttributedString{
+        let referenceText = text + " "
+        let authorName = userName
+        let attributedString = NSMutableAttributedString(string: "\(referenceText)\(authorName)",
+                                                         attributes: [
+                                                            .font: UIFont.systemFont(ofSize: 14.0, weight: .bold),
+                                                            .foregroundColor: UIColor(red: 21, green: 21, blue: 21),
+                                                            .kern: 0.39
+                                                         ])
+        attributedString.addAttribute(
+            .font,
+            value: UIFont.systemFont(ofSize: 14.0, weight: .regular),
+            range: NSRange(location: 0, length: referenceText.length)
+        )
+        return attributedString
+    }
+    
+    func getCreationDate(jsonData : BOUSApprovalDataResponseValues) -> String? {
+        if !jsonData.time_left.isEmpty{
+            let dateInFormate = jsonData.time_left.getdateFromStringFrom(dateFormat: "yyyy-MM-dd")
+            return "\(dateInFormate.day) \(dateInFormate.monthName.prefix(3))"
+        }else{
+            return ""
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "CommonFeeds",bundle: Bundle(for: CommonFeedsViewController.self))
         let dataValue = jsonDataValues[indexPath.row]
         let controller = storyboard.instantiateViewController(withIdentifier: "BOUSApprovalDetailViewController") as! BOUSApprovalDetailViewController
+        controller.isComingFromNominationPage = true
         controller.selectedNominationId = dataValue.id
         controller.requestCoordinator = requestCoordinator
         controller.mediaFetcher = mediaFetcher
@@ -119,3 +150,10 @@ class BOUSApprovalsListingViewController: UIViewController, UITableViewDelegate,
         topViewController?.present(vc, animated: false, completion: nil)
     }
 }
+
+extension String {
+    var length : Int {
+        return self.count
+    }
+}
+
