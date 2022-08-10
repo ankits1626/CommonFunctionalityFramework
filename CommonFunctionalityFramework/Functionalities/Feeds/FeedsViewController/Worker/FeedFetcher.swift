d70b20f1753bc3e0eccedfc21a61636c54f5ea63
suyeshkandpal28 <suyesh.kandpal@stu.upes.ac.in>
2022-08-10 09:56:20 +0530

    filter changes

--- CommonFunctionalityFramework/Functionalities/Feeds/FeedsViewController/Worker/FeedFetcher.swift
+++ CommonFunctionalityFramework/Functionalities/Feeds/FeedsViewController/Worker/FeedFetcher.swift
@@ -19,13 +19,18 @@ class FeedFetcher  {
         self.networkRequestCoordinator = networkRequestCoordinator
     }
     
-    func fetchFeeds(nextPageUrl : String?, feedType: String?, searchText: String?, completionHandler: @escaping FeedFetcherHandler) {
+    func fetchFeeds(nextPageUrl : String?, feedType: String?, searchText: String?,feedTypePk : Int = 0,organisationPK : Int = 0,departmentPK : Int = 0,dateRangePK : Int = 0,coreValuePk : Int = 0, completionHandler: @escaping FeedFetcherHandler) {
         if (commonAPICall == nil){
             self.commonAPICall = CommonAPICall(
                 apiRequestProvider: FeedFetchRequestGenerator(
                     feedID: nil,
                     nextPageUrl: nextPageUrl, feedType: feedType, searchText: searchText,
-                    networkRequestCoordinator: networkRequestCoordinator
+                    networkRequestCoordinator: networkRequestCoordinator,
+                    _feedTypePk: feedTypePk,
+                    _organisationPK: organisationPK,
+                    _departmentPK: departmentPK,
+                    _dateRangePK: dateRangePK,
+                    _coreValuePk: coreValuePk
                 ),
                 dataParser: FeedFetchDataParser(),
                 logouthandler: networkRequestCoordinator.getLogoutHandler()
@@ -36,13 +41,18 @@ class FeedFetcher  {
         })
     }
     
-    func fetchFeedDetail(_ feedID: Int,feedType: String, completionHandler: @escaping FeedFetcherHandler){
+    func fetchFeedDetail(_ feedID: Int,feedType: String,feedTypePk : Int = 0,organisationPK : Int = 0,departmentPK : Int = 0,dateRangePK : Int = 0,coreValuePk : Int = 0, completionHandler: @escaping FeedFetcherHandler){
         if (commonAPICall == nil){
             self.commonAPICall = CommonAPICall(
                 apiRequestProvider: FeedFetchRequestGenerator(
                     feedID: feedID,
                     nextPageUrl: nil, feedType: feedType, searchText: "",
-                    networkRequestCoordinator: networkRequestCoordinator
+                    networkRequestCoordinator: networkRequestCoordinator,
+                    _feedTypePk: feedTypePk,
+                    _organisationPK: organisationPK,
+                    _departmentPK: departmentPK,
+                    _dateRangePK: dateRangePK,
+                    _coreValuePk: coreValuePk
                 ),
                 dataParser: FeedFetchDataParser(),
                 logouthandler: networkRequestCoordinator.getLogoutHandler()
@@ -63,12 +73,23 @@ class FeedFetchRequestGenerator: APIRequestGeneratorProtocol  {
     private var feedType : String?
     private var searchText : String?
     
-    init( feedID: Int?, nextPageUrl : String?, feedType: String?, searchText: String?, networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol) {
+    var feedTypePk : Int = 0
+    var organisationPK : Int = 0
+    var departmentPK : Int = 0
+    var dateRangePK : Int = 0
+    var coreValuePk : Int = 0
+    
+    init( feedID: Int?, nextPageUrl : String?, feedType: String?, searchText: String?, networkRequestCoordinator: CFFNetworkRequestCoordinatorProtocol, _feedTypePk : Int, _organisationPK : Int, _departmentPK : Int, _dateRangePK : Int, _coreValuePk : Int) {
          self.feedID = feedID
         self.nextPageUrl = nextPageUrl
         self.feedType = feedType
         self.searchText = searchText
         self.networkRequestCoordinator = networkRequestCoordinator
+        self.feedTypePk = _feedTypePk
+        self.organisationPK = _organisationPK
+        self.departmentPK = _departmentPK
+        self.dateRangePK = _dateRangePK
+        self.coreValuePk = _coreValuePk
         urlBuilder = ParameterizedURLBuilder(baseURLProvider: networkRequestCoordinator.getBaseUrlProvider())
         requestBuilder = APIRequestBuilder(tokenProvider: networkRequestCoordinator.getTokenProvider())
     }
@@ -78,7 +99,7 @@ class FeedFetchRequestGenerator: APIRequestGeneratorProtocol  {
             if let searchedText = searchText {
                 if let baseUrl = networkRequestCoordinator.getBaseUrlProvider().baseURLString(){
                     let req =  self.requestBuilder.apiRequestWithHttpParamsAggregatedHttpParams(
-                        url: URL(string: baseUrl + "feeds/api/user_feed/?feed=\(feedType!)&search=\(searchedText)"),
+                        url: URL(string: baseUrl + "feeds/api/user_feed/?search=\(searchedText)"),
                         method: .GET,
                         httpBodyDict: nil
                     )
@@ -99,9 +120,16 @@ class FeedFetchRequestGenerator: APIRequestGeneratorProtocol  {
             else if let unwrappedNextPageUrl = nextPageUrl {
                 return self.requestBuilder.apiRequestWithHttpParamsAggregatedHttpParams(url: URL(string: unwrappedNextPageUrl), method: .GET, httpBodyDict: nil)
             }else{
+                var endPoints = "feeds/api/user_feed/?feed=\(feedType!)"
+                endPoints = appendFeedType(endPoints)
+                endPoints = appendFeedOrg(endPoints)
+                endPoints = appendFeedDepartment(endPoints)
+                endPoints = appendFeedDateRange(endPoints)
+                endPoints = appendFeedCoreValue(endPoints)
+                
                 if let baseUrl = networkRequestCoordinator.getBaseUrlProvider().baseURLString(){
                     let req =  self.requestBuilder.apiRequestWithHttpParamsAggregatedHttpParams(
-                        url: URL(string: baseUrl + "feeds/api/user_feed/?feed=\(feedType!)"),
+                        url: URL(string: baseUrl + endPoints),
                         method: .GET,
                         httpBodyDict: nil
                     )
@@ -112,6 +140,41 @@ class FeedFetchRequestGenerator: APIRequestGeneratorProtocol  {
                 return nil
             }
         }
+    }
+    
+    private func appendFeedType(_ baseEndpoint : String) -> String{
+        if self.feedTypePk != 0 {
+            return "\(baseEndpoint)&type=\(self.feedTypePk)"
+        }
+        return baseEndpoint
+    }
+    
+    private func appendFeedOrg(_ baseEndpoint : String) -> String{
+        if self.organisationPK != 0 {
+            return "\(baseEndpoint)&organization=\(self.organisationPK)"
+        }
+        return baseEndpoint
+    }
+    
+    private func appendFeedDepartment(_ baseEndpoint : String) -> String{
+        if self.departmentPK != 0 {
+            return "\(baseEndpoint)&department=\(self.departmentPK)"
+        }
+        return baseEndpoint
+    }
+    
+    private func appendFeedDateRange(_ baseEndpoint : String) -> String{
+        if self.dateRangePK != 0 {
+            return "\(baseEndpoint)&created_during=\(self.dateRangePK)"
+        }
+        return baseEndpoint
+    }
+    
+    private func appendFeedCoreValue(_ baseEndpoint : String) -> String{
+        if self.coreValuePk != 0 {
+            return "\(baseEndpoint)&user_strength=\(self.coreValuePk)"
+        }
+        return baseEndpoint
     }
 }
 
