ad4db2136d2c733af220488b06c43535bb250715
ankits1626 <ankit_seeking_job@yahoo.com>
2023-08-20 13:36:47 +0530

    joye app moved to common functionality

---
+++ CommonFunctionalityFramework/Functionalities/Joye/JoyAppWebViewController/JoyAppWebViewController.swift
@@ -0,0 +1,59 @@
+//
+//  JoyAppWebViewController.swift
+//  Flabuless
+//
+//  Created by Suyesh Kandpal on 13/09/22.
+//  Copyright © 2022 Rewardz Private Limited. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+public protocol JoyAppWebViewCompletionDone {
+    func completedWebViewProcess()
+}
+
+public class JoyAppWebViewController: UIViewController,WKUIDelegate,WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    @IBOutlet weak var webViewContainer: UIView!
+    public var joyAppBaseUrl : String!
+    var loader = MFLoader()
+    public var delegate : JoyAppWebViewCompletionDone?
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        setupWebView()
+    }
+    
+    func setupWebView() {
+        let webConfiguration = WKWebViewConfiguration()
+        let customFrame = CGRect.init(origin: CGPoint.zero, size: CGSize.init(width: 0.0, height: self.webViewContainer.frame.size.height))
+        self.webView = WKWebView (frame: customFrame , configuration: webConfiguration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webViewContainer.addSubview(webView)
+        webView.topAnchor.constraint(equalTo: webViewContainer.topAnchor).isActive = true
+        webView.rightAnchor.constraint(equalTo: webViewContainer.rightAnchor).isActive = true
+        webView.leftAnchor.constraint(equalTo: webViewContainer.leftAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: webViewContainer.bottomAnchor).isActive = true
+        webView.heightAnchor.constraint(equalTo: webViewContainer.heightAnchor).isActive = true
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        loadRequestInWebview(URLRequest(url: URL(string:joyAppBaseUrl)!))
+    }
+    
+    private func loadRequestInWebview(_ request : URLRequest){
+        webView.load(request)
+    }
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {}
+}
+
+//MARK:- Ibactions
+extension JoyAppWebViewController {
+    @IBAction func backButtonPressed(sender : UIButton) {
+        self.delegate?.completedWebViewProcess()
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
