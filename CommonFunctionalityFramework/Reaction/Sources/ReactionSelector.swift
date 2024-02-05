94eedb9538bf5491b5cbbaf61e69d3997c6e2c77
suyeshkandpal28 <suyesh.kandpal2@gmail.com>
2024-02-05 10:59:14 +0530

    [MANASA] Hide emoji changes

--- CommonFunctionalityFramework/Reaction/Sources/ReactionSelector.swift
+++ CommonFunctionalityFramework/Reaction/Sources/ReactionSelector.swift
@@ -41,8 +41,57 @@ public final class ReactionSelector: UIReactionControl {
   private var reactionIconLayers: [CALayer] = []
   private var reactionLabels: [UILabel]     = []
   private let backgroundLayer               = Components.reactionSelect.backgroundLayer()
-  private var _reactions: [Reaction]        = Reaction.facebook.all
+  private var _reactions: [Reaction]  = {
+        var reactions: [Reaction] = []
+        
+        // Add reactions based on user defaults
+      
+      if UserDefaults.standard.bool(forKey: "isPostAndPoll") {
+          if UserDefaults.standard.bool(forKey: "feedLike") {
+              reactions.append(Reaction.facebook.like)
+          }
 
+          if UserDefaults.standard.bool(forKey: "feedLove") {
+              reactions.append(Reaction.facebook.love)
+          }
+
+          if UserDefaults.standard.bool(forKey: "feedClap") {
+              reactions.append(Reaction.facebook.haha)
+          }
+
+          if UserDefaults.standard.bool(forKey: "feedSupport") {
+              reactions.append(Reaction.facebook.sad)
+          }
+
+          if UserDefaults.standard.bool(forKey: "feedCelebrate") {
+              reactions.append(Reaction.facebook.wow)
+          }
+
+          return reactions
+      } else {
+          if UserDefaults.standard.bool(forKey: "like") {
+              reactions.append(Reaction.facebook.like)
+          }
+
+          if UserDefaults.standard.bool(forKey: "love") {
+              reactions.append(Reaction.facebook.love)
+          }
+
+          if UserDefaults.standard.bool(forKey: "clap") {
+              reactions.append(Reaction.facebook.haha)
+          }
+
+          if UserDefaults.standard.bool(forKey: "support") {
+              reactions.append(Reaction.facebook.sad)
+          }
+
+          if UserDefaults.standard.bool(forKey: "celebrate") {
+              reactions.append(Reaction.facebook.wow)
+          }
+
+          return reactions
+      }
+    }()
   /**
    The reactions available in the selector.
    */
